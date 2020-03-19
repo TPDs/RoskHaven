b7ade917d0d880996229a106a818b0a3726acc9f
miklmaar <mikk01d3@stud.kea.dk>
2020-03-19 14:30:43 +0100

    tgf

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Farvel");
         System.out.println("hegsdf");
-        System.out.println("Christian er lort2");
+        System.out.println("Christiantgyucv er lort2");
 
 
     }
