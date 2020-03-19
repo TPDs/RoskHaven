b9d13baf80b37d6d809d24b2c5ccea7dcc15ac10
Christian <kreller99gmail.com>
2020-03-19 14:18:37 +0100

    Update Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hej hej");
+        System.out.println("Farvel");
         System.out.println("hegsdf");
         System.out.println("dsada");
         System.out.println(Double.MAX_VALUE);
