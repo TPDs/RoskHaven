8376aad734b6534bc1f95ee363696069d2aac970
miklmaar <mikk01d3@stud.kea.dk>
2020-03-20 21:22:06 +0100

    End of code-day 2
    Added worksheet and subclasses.

---
+++ src/com/company/WorkDay.java
@@ -0,0 +1,9 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class WorkDay {
+    //A WorkDay is assumed to be from 7:00-17:00, and is only splitted in full hours, meaning employees
+    //will have to be scheduled for a minumum of one hour and any subsequent full hour.
+    private ArrayList<WorkHour> workHours;
+}
--- src/com/company/Worksheet.java
+++ src/com/company/Worksheet.java
@@ -1,11 +1,20 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Worksheet implements ClassesToStoreInFiles {
+    ArrayList<WorkDay> workDays;
+    ArrayList<Employee> completeListOfEmployees;
+    ArrayList<DailyManager> completeListOfDailyManagers;
 
+    //Worksheets are a monthly schedule, that stores what employees will be scheduled to work at certain hours.
 
-
-
-
+    //This creates a new worksheet.
+    public Worksheet(InitiateArray ia){
+        workDays = new ArrayList<WorkDay>();
+        completeListOfEmployees = ia.employeeList;
+        completeListOfDailyManagers = ia.dailyManagerList;
+    }
 
 
     @Override
