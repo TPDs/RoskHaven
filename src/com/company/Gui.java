52ccf97ee0457fc5bb759ce71100f9ee4bb65d87
TPD <mig.mic@gmail.com>
2020-03-20 16:48:41 +0100

    Boss menu added + Gui Clear metode

--- src/com/company/Gui.java
+++ src/com/company/Gui.java
@@ -13,10 +13,14 @@ public class Gui implements ActionListener {
     static JButton button;
     static JTextField t_name;
     static JTextField t_password;
+    static JLabel l_name;
+    static JLabel l_password;
+    //static JSpinner ansatliste;
+    static JLabel logo;
+    static String user;
 
     Gui() {
     }
-
 
     public void gui() {
         frame = new JFrame("Roskilde frie børnehave");
@@ -26,32 +30,31 @@ public class Gui implements ActionListener {
 
         Image window_icon = Toolkit.getDefaultToolkit().getImage("src/resourser/window_icon.png");
         frame.setIconImage(window_icon);
-
+        frame.setLocationRelativeTo(null);
         Gui te = new Gui();
         button = new JButton("Login");
         button.setBounds(230, 256, 90, 25);
         button.addActionListener(te);
 
         String[] ansat = {"Boss", "Daglig leder", "pædagog"};
-        JComboBox ansatliste = new JComboBox(ansat);
-        ansatliste.setSelectedIndex(2);
-        ansatliste.addActionListener(this);
+
+        SpinnerListModel ansatliste = new SpinnerListModel(ansat);
+        JSpinner job = new JSpinner(ansatliste);
+
 
 
         l_test = new JLabel("Input fra navn+password");
-
-        JLabel l_name = new JLabel("Navn:");
-        JLabel l_password = new JLabel("Password:");
-
+        l_name = new JLabel("Navn:");
+        l_password = new JLabel("Password:");
         t_name = new JTextField("");
         t_password = new JTextField("");
 
         t_name.setBounds(190, 180, 130, 20);
         t_password.setBounds(190, 220, 130, 20);
-        ansatliste.setBounds(105, 256, 100, 25);
+        job.setBounds(105, 256, 100, 25);
         l_name.setBounds(105, 180, 150, 20);
         l_password.setBounds(105, 220, 150, 20);
-        l_test.setBounds(105, 300, 150, 20);
+        l_test.setBounds(105, 340, 150, 20);
 
 
         int x = 95;
@@ -59,12 +62,12 @@ public class Gui implements ActionListener {
         Image logo2;
         logo2 = new ImageIcon("src/resourser/kinder.png").getImage().getScaledInstance(x, y, Image.SCALE_SMOOTH);
 
-        JLabel logo = new JLabel();
+        logo = new JLabel();
         logo.setIcon(new ImageIcon(logo2));
         logo.setBounds(150, 50, x, y);
 
         frame.add(logo);
-        frame.add(ansatliste);
+        //frame.add(job);
         frame.add(l_test);
         frame.add(button);
         frame.add(t_name);
@@ -101,23 +104,25 @@ public class Gui implements ActionListener {
 
     }
 
-
     @Override
     public void actionPerformed(ActionEvent e) {
         String s = e.getActionCommand();
         if (s.equals("Login")) {
             l_test.setText(t_name.getText() + " " + t_password.getText());
             System.out.println("Test: " + t_password.getText() + " " + t_name.getText()); //ændres til login();
+            user = t_name.getText();
             t_name.setText("");
             t_password.setText("");
 
-        }
+            GuiClear clean = new GuiClear();
+            clean.guiClear();
 
+            GuiBoss boss = new GuiBoss();
+            boss.GuiBoss();
+
+
+        }
 
     }
 
-
 }
-
-
-
---
+++ src/com/company/GuiClear.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.awt.*;
+
+public class GuiClear {
+
+    public void guiClear() {
+
+       //Gui.frame.remove(Gui.job);
+        Gui.l_name.setVisible(false);
+        Gui.l_password.setVisible(false);
+        Gui.button.setVisible(false);
+        Gui.t_name.setVisible(false);
+        Gui.t_password.setVisible(false);
+        //Gui.ansatliste.setVisible(false);
+        Gui.l_name.setVisible(false);
+        // Gui.logo.setVisible(false);
+
+    }
+
+
+}
