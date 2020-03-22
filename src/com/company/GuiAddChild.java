c27a13eff46cf5edb3df9b4a257d898d90406ee3
Kreller99 <kreller99gmail.com>
2020-03-22 11:41:43 +0100

    test commit

--- src/com/company/GuiAddChild.java
+++ src/com/company/GuiAddChild.java
@@ -5,9 +5,29 @@ import java.awt.event.ActionListener;
 
 public class GuiAddChild extends GuiBoss implements ActionListener {
 
+    static JLabel child_name;
+    static JTextField c_name;
+    static JLabel child_birth;
+    static JTextField c_birth;
+    static JLabel child_cpr;
+    static JTextField c_cpr;
+    static JLabel child_address;
+    static JTextField c_address;
+    static JLabel child_note;
+    static JTextField c_note;
+    static JButton next;
+    //static String user;
+    //static String password;
+
     GuiAddChild(){}
 
     public void guiAddChild(){
         Gui.frame.setTitle("Roskilde frie børnehave - AddChild menu");
+
+
+
+
+
+
     }
 }
