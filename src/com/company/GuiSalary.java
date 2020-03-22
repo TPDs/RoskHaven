e1cfda15a0cef12bbbd6924a7612cf82dd15d4fd
Kreller99 <kreller99gmail.com>
2020-03-22 12:17:50 +0100

    Changed all Gui classes execpt Gui, added labels + other stuff to GuiAddChild

--- src/com/company/GuiSalary.java
+++ src/com/company/GuiSalary.java
@@ -7,6 +7,6 @@ public class GuiSalary extends GuiBoss implements ActionListener {
     GuiSalary(){}
 
     public void guiSalary(){
-        Gui.frame.setTitle("Roskilde frie børnehave - Salary menu");
+        Gui.frame.setTitle("Roskilde frie børnehave - Salary");
     }
 }
