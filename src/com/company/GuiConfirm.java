a84ce8649f5578a354560e6199bfedf8cb7910a5
Michael Petersen <mig.mic@gmail.com>
2020-03-24 15:17:21 +0100

    AddConfirm , Små rettelser

--- src/com/company/GuiConfirm.java
+++ src/com/company/GuiConfirm.java
@@ -1,13 +1,41 @@
 package com.company;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class GuiConfirm {
+    static JTextArea box_info;
+    static JLabel confirm_lable;
+    static JButton confirm_button;
 
+    GuiConfirm() {
 
+    }
 
 
     public void guiConfirm() {
         Gui.frame.setTitle("Roskilde frie børnehave - Godkend");
-        System.out.println("Confirm Gui loaded....");
+
+        GuiConfirm te = new GuiConfirm();
+
+        box_info = new JTextArea();
+        confirm_lable = new JLabel("Godkend information");
+        confirm_button = new JButton("Godkend");
+
+
+        box_info.setLineWrap(true);
+        Gui.frame.getContentPane().add(confirm_lable, BorderLayout.NORTH);
+        Gui.frame.getContentPane().add(confirm_button, BorderLayout.CENTER);
+
+        confirm_lable.setBounds(130, 40, 150, 20);
+        box_info.setBounds(50, 80, 280, 200);
+        confirm_button.setBounds(275, 320, 95, 20);
+
+
+        Gui.frame.add(box_info);
+        Gui.frame.add(confirm_lable);
+        Gui.frame.add(confirm_button);
+
 
     }
 }
