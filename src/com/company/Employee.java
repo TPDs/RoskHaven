7ca05d818a7e307b6c981c55c8a376451f1bf109
miklmaar <mikk01d3@stud.kea.dk>
2020-03-27 15:30:57 +0100

    Users now require a CPR as a unique identifier, not a uniquely made ID.

--- src/com/company/Employee.java
+++ src/com/company/Employee.java
@@ -2,12 +2,6 @@ package com.company;
 
 public class Employee extends User {
     public Employee(String name, String id, String password) {
-        super(name, id, password);
+        super(name, "", password);
     }
 }
-/*
-* Hours:Double
-* Salery:Double
-* MaxHours:Double
-* phoneNumber:int
-* */
