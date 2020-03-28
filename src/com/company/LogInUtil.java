ae490f04caa917154ab0a64fd139325f0971c372
miklmaar <mikk01d3@stud.kea.dk>
2020-03-28 17:20:25 +0100

    LogInUtil method updated

--- src/com/company/LogInUtil.java
+++ src/com/company/LogInUtil.java
@@ -8,26 +8,31 @@ public class LogInUtil {
 
     }
 
-    public static boolean logIn(String id, String password){
+    public boolean bossLogIn(String CPR, String password) {
         Boss boss = Kindergarten.getInstance().getBoss();
-        if(id.equals(boss.getCPR())){
+        if (CPR.equals(boss.getCPR())) {
             return boss.login(password);
         }
+        return false;
+    }
 
+    public boolean dmLogIn(String CPR, String password) {
         ArrayList<DailyManager> dailyManagers = Kindergarten.getInstance().getDailyManagersInGarten();
-        for(int i=0; i<dailyManagers.size(); i++){
-            if(id.equals(dailyManagers.get(i).getCPR())){
+        for (int i = 0; i < dailyManagers.size(); i++) {
+            if (CPR.equals(dailyManagers.get(i).getCPR())) {
                 return dailyManagers.get(i).login(password);
             }
         }
+        return false;
+    }
 
+    public boolean empLogIn(String CPR, String password){
         ArrayList<Employee> employees = Kindergarten.getInstance().getEmployeesInGarten();
         for(int i=0; i<employees.size(); i++){
-            if(id.equals(employees.get(i).getCPR())){
+            if(CPR.equals(employees.get(i).getCPR())){
                 return employees.get(i).login(password);
             }
         }
-
         return false;
     }
 }
